f66aeffb5b0b78221ff992be75c4c39248a49dbb
tampvnfct31067-sketch <tampvnfct31067@gmail.com>
2025-11-08 02:11:00 +0700

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ async function sendMessage() {
 
   try {
     const res = await fetch(
-      "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-pro-latest:generateContent?key=" + API_KEY,
+      "https://generativelanguage.googleapis.com/v1beta1/models/gemini-2.5-pro-latest:generateContent?key=" + API_KEY,
       {
         method: "POST",
         headers: { "Content-Type": "application/json" },
