1dfdb742660429c8f47fe1a728c219684efd0aec
NGU24H <tampvnfct31067@gmail.com>
2025-11-09 23:24:21 +0700

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ async function sendMessage() {
 
   try {
     const res = await fetch(
-      "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-pro-exp-02-05:generateContent?key=" + API_KEY,
+      "https://generativelanguage.googleapis.com/v1/models/gemini-2.0-pro-exp-02-05:generateContent?key=" + API_KEY,
       {
         method: "POST",
         headers: { "Content-Type": "application/json" },
